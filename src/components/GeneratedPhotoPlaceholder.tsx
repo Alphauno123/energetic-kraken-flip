3273861be6c8dca9c95bad4a688b249416c83cca
[dyad] <git@dyad.sh>
2025-11-29 23:47:31 -0400

    [dyad] wrote 2 file(s)

--- src/components/GeneratedPhotoPlaceholder.tsx
+++ src/components/GeneratedPhotoPlaceholder.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { cn } from '@/lib/utils';
-import { styleIcons, styleBackgroundClasses, getStyleNameById } from '@/utils/styles'; // Import from consolidated utility file
+import { styleIcons, styleBackgroundClasses } from '@/utils/styles'; // Removed getStyleNameById
 // Removed: import { Square } from 'lucide-react'; // Only import Square for default, as others are in styleIcons
 
 interface GeneratedPhotoPlaceholderProps {
